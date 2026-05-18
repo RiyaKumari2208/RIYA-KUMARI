054a06c6f7d6e97430d7cafdaf221ee1e68d246d
Riya Kumari <riya112006222gmail.comgit config --global user.email correct>
2026-05-18 21:13:18 +0530

    odd numbers

--- JAVA SCRIPT DAY 1/Question1.js
+++ JAVA SCRIPT DAY 1/Question1.js
@@ -1,6 +1,6 @@
 // CAP to find all even numbers between 1 to 100
 
-for(let i=1;i<100;i++){
+for(let i=1;i<101;i++){
     if(i%2==0)
         console.log(i)
 }
---
+++ JAVA SCRIPT DAY 1/Question2.js
@@ -0,0 +1,6 @@
+//CAP to  find all odd number between 1 to 100
+
+for(let i=1;i<=100;i++){
+    if(i%2!=0)
+        console.log(i)
+}
