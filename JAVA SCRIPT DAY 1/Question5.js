672ad2b4cae2955eefe0989c5bea9d25158863cf
Riya Kumari <riya112006222gmail.comgit config --global user.email correct>
2026-05-19 21:45:18 +0530

    Prime

---
+++ JAVA SCRIPT DAY 1/Question5.js
@@ -0,0 +1,15 @@
+// another method of prime
+
+for (let num = 2; num <= 100; num++) {
+  let isprime = true;
+
+  for (let i = 2; i < num; i++) {
+    if (num % i == 0) {
+      isprime = false;
+      break;
+    }
+  }
+  if (isprime) {
+    console.log(num);
+  }
+}
