699dbaa30ba79e0feb51cb6ae21471aff17aca10
Riya Kumari <riya112006222gmail.comgit config --global user.email correct>
2026-05-25 20:51:57 +0530

    Module

---
+++ Server day4/Module/iseven.js
@@ -0,0 +1,9 @@
+function iseven(num) {
+  if (num % 2 == 0) {
+    console.log("even");
+  } else {
+    console.log("odd");
+  }
+}
+
+module.exports = iseven;
---
+++ Server day4/index.js
@@ -0,0 +1,8 @@
+const iseven = require("./Module/iseven");
+iseven(23);
+
+const isodd = require("./Module/isodd");
+isodd(25);
+
+const isprime = require("./Module/isprime");
+isprime(27);
