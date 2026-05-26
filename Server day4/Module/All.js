b2b5f9c7c5b42a5ba391b067211b8bd267958477
Riya Kumari <riya112006222gmail.comgit config --global user.email correct>
2026-05-26 15:24:20 +0530

    package

---
+++ Server day4/Module/All.js
@@ -0,0 +1,27 @@
+function iseven(num) {
+  if (num % 2 == 0) {
+    console.log("even");
+  } else {
+    console.log("odd");
+  }
+}
+function isodd(num) {
+  if (num % 2 != 0) {
+    console.log("odd");
+  } else {
+    console.log("even");
+  }
+}
+function isprime(n) {
+  let count = 0;
+  for (let i = 1; i <= n; i++)
+    if (n % i == 0) {
+      count++;
+    }
+  if (count == 2) {
+    console.log(n);
+  } else {
+    console.log("not prime");
+  }
+}
+module.exports = { iseven: event, isodd: odd, isprime: prime };
--- Server day4/index.js
+++ Server day4/index.js
@@ -1,8 +1,9 @@
-const iseven = require("./Module/iseven");
-iseven(23);
+const http = require("http");
 
-const isodd = require("./Module/isodd");
-isodd(25);
-
-const isprime = require("./Module/isprime");
-isprime(27);
+const server = http.createServer((req, res) => {
+  res.write("hello world");
+  res.end();
+});
+server.listen(3000, () => {
+  console.log("Server run on port 3000");
+});
