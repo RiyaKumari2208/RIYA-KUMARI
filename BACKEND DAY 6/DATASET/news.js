53c3bb9799f177965143df7b10aea6026d6ab75c
Riya Kumari <riya112006222gmail.comgit config --global user.email correct>
2026-05-28 15:25:19 +0530

    day6

---
+++ BACKEND DAY 6/DATASET/news.js
@@ -0,0 +1,104 @@
+const newsdata = [
+  {
+    id: 1,
+    news: "Scientists Discover Chocolate Cures All Diseases",
+    category: "General",
+  },
+  {
+    id: 2,
+    news: "Aliens Open Embassy in New York",
+    category: "General",
+  },
+  {
+    id: 3,
+    news: "Government Announces Free Cars for Every Citizen",
+    category: "Politics",
+  },
+  {
+    id: 4,
+    news: "Drinking Coffee Extends Human Life to 150 Years",
+    category: "General",
+  },
+  {
+    id: 5,
+    news: "Dinosaurs Found Alive in Amazon Rainforest",
+    category: "General",
+  },
+  {
+    id: 6,
+    news: "Moon Made Entirely of Cheese, NASA Confirms",
+    category: "General",
+  },
+  {
+    id: 7,
+    news: "Social Media App Pays Users $1000 Per Day",
+    category: "General",
+  },
+  {
+    id: 8,
+    news: "Invisible Cloak Available in Local Markets",
+    category: "General",
+  },
+  {
+    id: 9,
+    news: "Eating Ice Cream Daily Guarantees Weight Loss",
+    category: "General",
+  },
+  {
+    id: 10,
+    news: "World to End Next Tuesday, Experts Warn",
+    category: "General",
+  },
+  {
+    id: 11,
+    news: "National Football Team Wins World Cup Without Playing",
+    category: "Sports",
+  },
+  {
+    id: 12,
+    news: "Cricketer Scores 1000 Runs in One Over",
+    category: "Sports",
+  },
+  {
+    id: 13,
+    news: "Olympics Introduces Sleeping Competition",
+    category: "Sports",
+  },
+  {
+    id: 14,
+    news: "Prime Minister Announces Tax-Free Country Forever",
+    category: "Politics",
+  },
+  {
+    id: 15,
+    news: "Parliament Approves Law Requiring Everyone to Dance Daily",
+    category: "Politics",
+  },
+  {
+    id: 16,
+    news: "Cats Elected as Mayors in Three Cities",
+    category: "Politics",
+  },
+  {
+    id: 17,
+    news: "Internet Will Shut Down Permanently Tomorrow",
+    category: "General",
+  },
+  {
+    id: 18,
+    news: "New Pill Allows Humans to Breathe Underwater",
+    category: "General",
+  },
+  {
+    id: 19,
+    news: "Volcano Eruption Causes Rain of Gold",
+    category: "General",
+  },
+  {
+    id: 20,
+    news: "Artificial Intelligence Declares Independence",
+    category: "General",
+  },
+];
+
+module.exports = newsdata;
