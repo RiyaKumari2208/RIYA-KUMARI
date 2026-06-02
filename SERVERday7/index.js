3df0f0d8e4c6aa584eec1ad0dd371b44ad5c9172
Riya Kumari <riya112006222gmail.comgit config --global user.email correct>
2026-06-02 14:43:50 +0530

    APIs

--- SERVERday7/index.js
+++ SERVERday7/index.js
@@ -3,12 +3,29 @@ const news = require("./Dataset/news2");
 
 const app = express();
 
+const { searchByTerm, searchByCategory } = require("./Module/utils");
+
 app.get("/", (req, res) => {
   res.send("Welcome to news website");
 });
 
 app.get("/news2", (req, res) => {
-  res.json(news);
+  const { search, category, limit } = req.query;
+  let filternews = news;
+  if (search) {
+    filternews = searchByTerm(search, news);
+  }
+  if (category) {
+    filternews = searchByCategory(category, filternews);
+  }
+  if (limit) {
+    filternews = filternews.slice(0, limit);
+  }
+  if (filternews.length > 0) {
+    return res.send(filternews);
+  } else {
+    return res.send("News Not Found, please try some different filters");
+  }
 });
 
 app.get("/news2/:id", (req, res) => {
@@ -19,65 +36,6 @@ app.get("/news2/:id", (req, res) => {
     }
   }
   return res.json({ status: "not found" });
-});
-
-app.get("/news2", (req, res) => {
-  const limit = req.query.limit;
-  let result = news;
-
-  if (limit) {
-    result = result.slice(0, limit);
-  }
-  res.json(result);
-});
-
-app.get("/news2", (req, res) => {
-  const limit = req.query.limit;
-  const category = req.query.category;
-  if (limit) {
-    return res.json(news.slice(0, limit));
-  } else if (category) {
-    const finalnews = [];
-    for (let i = 0; i < news.length; i++) {
-      if (news[i].category.toLowerCase() === category.toLowerCase()) {
-        finalnews.push(news[i]);
-      }
-    }
-    return res.json(finalnews);
-  } else {
-    return res.json(news);
-  }
-});
-
-app.get("/news2", (req, res) => {
-  const category = req.query.category;
-  const limit = req.query.limit;
-
-  let result = news;
-
-  if (category) {
-    result = result.filter((i) => i.category === category);
-  }
-  if (limit) {
-    result = result.slice(0, Number(limit));
-  }
-  res.json(result);
-});
-
-app.get("/news2", (req, res) => {
-  const limit = Number(req.query.limit);
-  const from = req.query.from;
-  let result = news;
-
-  if (limit) {
-    if (from === "bottom") {
-      result = result.slice(-limit);
-    } else {
-      result = result.slice(0, limit);
-    }
-  }
-
-  res.json(result);
 });
 
 app.listen(3000, () => {
